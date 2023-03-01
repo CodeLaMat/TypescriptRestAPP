45969b128b7f8eed6c3dd79eefed0462d7edf958
CodeLaMat <eyvaz.alishov@edu.bc.fi>
2023-03-01 11:17:50 +0200

    post and get methods tested

---
+++ src/controller/todo.ts
@@ -0,0 +1,16 @@
+import { RequestHandler } from "express";
+import { Todo } from "../modals/todo";
+
+const TODOS: Todo[] = [];
+
+export const createTodo: RequestHandler = (req, res, next) => {
+  const text = (req.body as { text: string }).text;
+  const newTodo = new Todo(Math.random().toString(), text);
+  TODOS.push(newTodo);
+
+  res.status(201).json({ message: "Create the todo", createTodo: newTodo });
+};
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.json({ todos: TODOS });
+};
